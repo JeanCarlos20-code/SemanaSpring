988f82b8b0dc09ef62df8555996e5f1412752bd7
Jean Carlos <jeancarlos272010@gmail.com>
2022-03-11 18:32:25 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,28 @@
-import Navebar from "./components/Navebar";
+import {
+  BrowserRouter,
+  Routes,
+  Route //usado para configurar as rotas
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navebar/>
+    //BrowserRouter inicia a configuração das rotas
+    //Navbar foi colocado antes das rotas pois é a página principal 
+    //routes irá configurar rota por rota
+    //route no caminho / para abrir no elemento listing
+    //tem uma subrota no form para chamar filme 1, filme 2 e etc
+    <BrowserRouter>  
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
