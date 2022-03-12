01beea02813c7b9a1f2b1f68102b6908de15afd5
Jean Carlos <jeancarlos272010@gmail.com>
2022-03-11 22:52:08 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -5,7 +5,7 @@ function Pagination() {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={true} >
+                <button className="dsmovie-pagination-button" disabled={true} > 
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
                 <p>{`${1} de ${3}`}</p>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,37 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
 
 function Listing() {
     return (
-        <Pagination />
+        //aceitar as duas funções
+        //mb -> margin bottom
+        //bootstrap -> col-sm-6 vai ocupar 6 colunas das 12 da grid, ou seja cada card irá ocupar metade da telao conteiner
+        <>
+            <Pagination />
+
+            <div className="containter">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+
     );
 }
 
